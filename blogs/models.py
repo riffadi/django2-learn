2a379a90bcf5c105a01fc7cf8a441e6b39b3c294
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-08-07 13:53:27 +0700

    add magic function __str__

--- blogs/models.py
+++ blogs/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
 	title = models.CharField(max_length=100)
 	desc = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
